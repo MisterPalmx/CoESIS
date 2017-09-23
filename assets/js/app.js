204309227a3857a88769730191777cc7bac89f26
MisterPalmx <palmz.hacker@gmail.com>
2017-09-24 03:02:38 +0700

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,185 @@
+var app = angular.module("app", ["ui.router"]);
+
+app.constant("API_URL", "//localhost:5000");
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+	$urlRouterProvider.otherwise("/");
+
+	$stateProvider
+	.state("studentList", {
+		url: "/",
+		templateUrl: "views/studentList.html",
+    controller: "studentListCtrl",
+	})
+	.state("studentNew", {
+		url: "/student/new",
+		templateUrl: "views/studentNew.html",
+    controller: "studentNewCtrl",
+	})
+	.state("studentView", {
+		url: "/student/:id",
+		templateUrl: "views/studentView.html",
+    controller: "studentViewCtrl",
+	})
+	.state("eventList", {
+		url: "/event",
+		templateUrl: "views/eventList.html",
+    controller: "eventListCtrl",
+	})
+	.state("eventNew", {
+		url: "/event/new",
+		templateUrl: "views/eventNew.html",
+    controller: "eventNewCtrl",
+	})
+	.state("eventView", {
+		url: "/event/:id",
+		templateUrl: "views/eventView.html",
+    controller: "eventViewCtrl",
+	})
+	.state("summary", {
+		url: "/summary",
+		templateUrl: "views/summary.html",
+    controller: "summaryCtrl",
+	})
+
+	$locationProvider.html5Mode(false);
+
+});
+
+app.run(function($rootScope, $state, $http, API_URL, $window, $q) {
+
+  $rootScope.preloader = true;
+	$rootScope.students = [];
+	$rootScope.events = [];
+
+	var promises = [],
+		studentP = $q.defer(),
+		eventP = $q.defer();
+
+	promises.push(studentP.promise);
+	promises.push(eventP.promise);
+
+	$http.get(API_URL + "/student")
+	.success(function(response) {
+		studentP.resolve();
+		if (response.success) {
+			$rootScope.students = response.data.students;
+		}
+	});
+
+	$http.get(API_URL + "/event")
+	.success(function(response) {
+		eventP.resolve();
+		if (response.success) {
+			$rootScope.events = response.data.events;
+		}
+	});
+
+	$q.all(promises).then(function() {
+		$rootScope.preloader = false;
+		console.log("Students", $rootScope.students);
+		console.log("Events", $rootScope.events);
+	});
+
+});
+
+app.controller("studentListCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
+
+});
+
+app.controller("studentNewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
+
+	$scope.add = function() {
+		$scope.loading = true;
+		$http.post(API_URL + "/student/new", {
+			id: $scope.id,
+			nickname: $scope.nickname,
+			name: $scope.name,
+			gender: $scope.gender,
+			phone: $scope.phone,
+			phone2: $scope.phone2,
+			address: $scope.address,
+			facebook: $scope.facebook,
+		})
+		.success(function(response) {
+			$scope.loading = false;
+			if (response.success) {
+				alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
+				$rootScope.preloader = true;
+				$http.get(API_URL + "/student")
+				.success(function(response) {
+					$rootScope.preloader = false;
+					$state.go("studentList");
+					if (response.success) {
+						$rootScope.students = response.data.students;
+					}
+				});
+			} else {
+				alert("Error! " + response.message);
+			}
+		})
+	}
+});
+
+app.controller("studentViewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state, $stateParams) {
+	$scope.id = $stateParams.id;
+	$scope.student = {};
+	$scope.event = {};
+
+	$scope.$watch('$root.students', function() {
+		angular.forEach($rootScope.students, function(student) {
+			if (student.id == $scope.id)
+				$scope.student = student;
+		});
+	});
+
+});
+
+app.controller("eventListCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
+
+});
+
+app.controller("eventNewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
+	$scope.add = function() {
+		$scope.loading = true;
+		$http.post(API_URL + "/event/new", {
+			name: $scope.name,
+			description: $scope.description,
+		})
+		.success(function(response) {
+			$scope.loading = false;
+			if (response.success) {
+				alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
+				$rootScope.preloader = true;
+				$http.get(API_URL + "/event")
+				.success(function(response) {
+					$rootScope.preloader = false;
+					$state.go("eventList")
+					if (response.success) {
+						$rootScope.events = response.data.events;
+					}
+				});
+			} else {
+				alert("Error! " + response.message);
+			}
+		})
+	}
+});
+
+app.controller("eventViewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state, $stateParams) {
+	$scope.id = $stateParams.id;
+	$scope.event = {};
+
+	$scope.$watch('$root.events', function() {
+		angular.forEach($rootScope.events, function(event) {
+			if (event.id == $scope.id)
+				$scope.event = event;
+		});
+	});
+});
+
+
+app.controller("summaryCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
+
+});
---
+++ server.js
@@ -0,0 +1,221 @@
+var mysql = require('mysql');
+var express = require('express');
+var async = require('async');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.listen(5000);
+console.log('Server is running on http://localhost:5000')
+
+var conn = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'CoESIS',
+  connectionLimit: 10
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	req.ipv4 = req.headers['x-real-ip'] || req.headers['x-client-ip'] || req.headers['x-forwarded-for'] || '127.0.0.1';
+	next();
+});
+
+app.get('/student', (req, res) => {
+  var connection,
+    students = [];
+  async.series([
+    (callback) => {
+      conn.getConnection((err, response) => {
+        if (err) return callback(err);
+        connection = response;
+        callback();
+      })
+    },
+    (callback) => {
+      connection.query('select * from student', (err, response) => {
+        if (err) return callback(err);
+        students = response;
+        callback();
+      })
+    }
+  ], (err) => {
+    if (connection) connection.release();
+    if (err) return res.json({
+      success: false,
+      message: err
+    })
+    res.json({
+      success: true,
+      data: {
+          students: students
+      }
+    })
+  })
+})
+
+app.post('/student/new', (req, res) => {
+  var connection,
+    id = req.body.id,
+    nickname = req.body.nickname,
+    name = req.body.name,
+    gender = req.body.gender,
+    phone = req.body.phone,
+    phone2 = req.body.phone2,
+    address = req.body.address,
+    facebook = req.body.facebook;
+
+  async.series([
+    (callback) => {
+      conn.getConnection((err, response) => {
+        if (err) return callback(err);
+        connection = response;
+        callback();
+      })
+    },
+    (callback) => {
+      connection.query('insert into student (id, nickname, name, gender, phone, phone2, address, facebook, registered) values (?, ?, ?, ?, ?, ?, ?, ?, now())',
+      [ id, nickname, name, gender, phone, phone2, address, facebook ],
+      (err, response) => {
+        if (err) return callback(err);
+        callback();
+      })
+    }
+  ], (err) => {
+    if (connection) connection.release();
+    if (err) return res.json({
+      success: false,
+      message: err
+    })
+    res.json({
+      success: true
+    })
+  })
+})
+
+// app.get('/student/:id', (req, res) => {
+//   var connection,
+//     id = req.params.id,
+//     student = {};
+//   async.series([
+//     (callback) => {
+//       conn.getConnection((err, response) => {
+//         if (err) return callback(err);
+//         connection = response;
+//         callback();
+//       })
+//     },
+//     (callback) => {
+//       connection.query('select * from student where id = ' + mysql.escape(id), (err, response) => {
+//         if (err) return callback(err);
+//         student = response[0];
+//         callback();
+//       })
+//     }
+//   ], (err) => {
+//     if (connection) connection.release();
+//     if (err) return res.json({
+//       success: false,
+//       message: err
+//     })
+//     res.json({
+//       success: true,
+//       data: {
+//           student: student
+//       }
+//     })
+//   })
+// })
+
+app.get('/event', (req, res) => {
+  var connection,
+    events = [],
+    participants = [];
+
+  async.series([
+    (callback) => {
+      conn.getConnection((err, response) => {
+        if (err) return callback(err);
+        connection = response;
+        callback();
+      })
+    },
+    (callback) => {
+      async.parallel([
+        (callback) => {
+          connection.query('select * from event', (err, response) => {
+            if (err) return callback(err);
+            events = response;
+            callback();
+          })
+        },
+        (callback) => {
+          connection.query('select * from participant', (err, response) => {
+            if (err) return callback(err);
+            participants = response;
+            callback();
+          })
+        }
+      ], callback);
+    }
+  ], (err) => {
+    if (connection) connection.release();
+    if (err) return res.json({
+      success: false,
+      message: err
+    })
+    events.forEach((event) => {
+      event.participants = [];
+      participants.forEach((participant) => {
+        if (participant.event_id == event.id)
+          event.participants.push(participant);
+      })
+    })
+    res.json({
+      success: true,
+      data: {
+          events: events
+      }
+    })
+  })
+})
+
+
+app.post('/event/new', (req, res) => {
+  var connection,
+    name = req.body.name,
+    description = req.body.description;
+
+  async.series([
+    (callback) => {
+      conn.getConnection((err, response) => {
+        if (err) return callback(err);
+        connection = response;
+        callback();
+      })
+    },
+    (callback) => {
+      connection.query('insert into event (name, description, created) values (?, ?, now())',
+      [ name, description ],
+      (err, response) => {
+        if (err) return callback(err);
+        callback();
+      })
+    }
+  ], (err) => {
+    if (connection) connection.release();
+    if (err) return res.json({
+      success: false,
+      message: err
+    })
+    res.json({
+      success: true
+    })
+  })
+})
