b0a6f3204c759516eb4d1f2cab765afc38d87ca5
MisterPalmx <palmz.hacker@gmail.com>
2017-09-27 01:56:36 +0700

    add modal, remove duplicate function (getStatus, getRemark, getAddtime), make GUI more responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,86 +1,158 @@
-var app = angular.module("app", ["ui.router"]);
+var app = angular.module("app", ["ui.router", "ui.bootstrap"]);
 
-app.constant("API_URL", "//localhost:5000");
+app.constant("API_URL", "//192.168.1.2:5000");
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
-	$urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise("/");
 
-	$stateProvider
-	.state("studentList", {
-		url: "/",
-		templateUrl: "views/studentList.html",
-    controller: "studentListCtrl",
-	})
-	.state("studentNew", {
-		url: "/student/new",
-		templateUrl: "views/studentNew.html",
-    controller: "studentNewCtrl",
-	})
-	.state("studentView", {
-		url: "/student/:id",
-		templateUrl: "views/studentView.html",
-    controller: "studentViewCtrl",
-	})
-	.state("eventList", {
-		url: "/event",
-		templateUrl: "views/eventList.html",
-    controller: "eventListCtrl",
-	})
-	.state("eventNew", {
-		url: "/event/new",
-		templateUrl: "views/eventNew.html",
-    controller: "eventNewCtrl",
-	})
-	.state("eventView", {
-		url: "/event/:id",
-		templateUrl: "views/eventView.html",
-    controller: "eventViewCtrl",
-	})
-	.state("summary", {
-		url: "/summary",
-		templateUrl: "views/summary.html",
-    controller: "summaryCtrl",
-	})
+  $stateProvider
+    .state("studentList", {
+      url: "/",
+      templateUrl: "views/studentList.html",
+      controller: "studentListCtrl",
+    })
+    .state("studentNew", {
+      url: "/student/new",
+      templateUrl: "views/studentNew.html",
+      controller: "studentNewCtrl",
+    })
+    .state("studentView", {
+      url: "/student/:id",
+      templateUrl: "views/studentView.html",
+      controller: "studentViewCtrl",
+    })
+    .state("eventList", {
+      url: "/event",
+      templateUrl: "views/eventList.html",
+      controller: "eventListCtrl",
+    })
+    .state("eventNew", {
+      url: "/event/new",
+      templateUrl: "views/eventNew.html",
+      controller: "eventNewCtrl",
+    })
+    .state("eventView", {
+      url: "/event/:id",
+      templateUrl: "views/eventView.html",
+      controller: "eventViewCtrl",
+    })
+    .state("summary", {
+      url: "/summary",
+      templateUrl: "views/summary.html",
+      controller: "summaryCtrl",
+    })
 
-	$locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true);
 
 });
 
-app.run(function($rootScope, $state, $http, API_URL, $window, $q) {
+app.run(function($rootScope, $state, $http, API_URL, $window, $q, $uibModal) {
 
   $rootScope.preloader = true;
-	$rootScope.students = [];
-	$rootScope.events = [];
+  $rootScope.students = [];
+  $rootScope.events = [];
 
-	var promises = [],
-		studentP = $q.defer(),
-		eventP = $q.defer();
+  var promises = [],
+    studentP = $q.defer(),
+    eventP = $q.defer();
 
-	promises.push(studentP.promise);
-	promises.push(eventP.promise);
+  promises.push(studentP.promise);
+  promises.push(eventP.promise);
 
-	$http.get(API_URL + "/student")
-	.success(function(response) {
-		studentP.resolve();
-		if (response.success) {
-			$rootScope.students = response.data.students;
-		}
-	});
+  $http.get(API_URL + "/student")
+    .success(function(response) {
+      studentP.resolve();
+      if (response.success) {
+        $rootScope.students = response.data.students;
+      }
+    });
 
-	$http.get(API_URL + "/event")
-	.success(function(response) {
-		eventP.resolve();
-		if (response.success) {
-			$rootScope.events = response.data.events;
-		}
-	});
+  $http.get(API_URL + "/event")
+    .success(function(response) {
+      eventP.resolve();
+      if (response.success) {
+        $rootScope.events = response.data.events;
+      }
+    });
 
-	$q.all(promises).then(function() {
-		$rootScope.preloader = false;
-		console.log("Students", $rootScope.students);
-		console.log("Events", $rootScope.events);
-	});
+  $q.all(promises).then(function() {
+    $rootScope.preloader = false;
+    console.log("Students", $rootScope.students);
+    console.log("Events", $rootScope.events);
+  });
+
+	$rootScope.getDataByStudent = function(event, student) {
+		var data = {
+			status: 0,
+			remark: null,
+			addtime: null,
+		};
+		angular.forEach(event.participants, function(participant) {
+			if (participant.studentId == student.id) {
+				data = {
+					status: participant.status,
+					remark: participant.remark,
+					addtime: participant.addtime
+				};
+				if (!participant.status)
+					data.addtime = null;
+			}
+		});
+		return data;
+	}
+
+	$rootScope.updateEvent = function(event, student) {
+	  var modalInstance = $uibModal.open({
+	    animation: true,
+	    templateUrl: 'views/modals/update.html',
+			resolve: {
+				event: function() {
+					return event;
+				},
+				student: function() {
+					return student;
+				}
+			},
+			controller: function($scope, $uibModalInstance, event, student) {
+				$scope.event = event;
+				$scope.student = student;
+				$scope.password = 1;
+				$scope.formData = {
+					studentId: student.id,
+					status: $rootScope.getDataByStudent(event, student).status, // Get default value and set default value to radiobutton
+					remark: $rootScope.getDataByStudent(event, student).remark
+				};
+
+			  $scope.update = function() {
+					console.log($scope.formData);
+			    $http.post(API_URL + "/event/" + $scope.event.id + "/update", $scope.formData)
+			      .success(function(response) {
+			        $scope.loading = false;
+			        if (response.success) {
+			          // $rootScope.preloader = true; reload in background
+			          $http.get(API_URL + "/event")
+			            .success(function(response) {
+			              // $rootScope.preloader = false;
+			              if (response.success) {
+			                $rootScope.events = response.data.events;
+			              }
+										$uibModalInstance.dismiss();
+			            });
+			        } else {
+			          alert("Error! " + response.message);
+			          $state.reload(); // reload checkbox
+			        }
+			      })
+			  }
+
+				$scope.close = function() {
+					$uibModalInstance.dismiss();
+				}
+			}
+	  });
+	}
+
 
 });
 
@@ -90,229 +162,104 @@ app.controller("studentListCtrl", function($rootScope, $scope, $http, API_URL, $
 
 app.controller("studentNewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
 
-	$scope.add = function() {
-		$scope.loading = true;
-		$http.post(API_URL + "/student/new", {
-			id: $scope.id,
-			nickname: $scope.nickname,
-			name: $scope.name,
-			gender: $scope.gender,
-			phone: $scope.phone,
-			phone2: $scope.phone2,
-			address: $scope.address,
-			facebook: $scope.facebook,
-		})
-		.success(function(response) {
-			$scope.loading = false;
-			if (response.success) {
-				alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
-				$rootScope.preloader = true;
-				$http.get(API_URL + "/student")
-				.success(function(response) {
-					$rootScope.preloader = false;
-					$state.go("studentList");
-					if (response.success) {
-						$rootScope.students = response.data.students;
-					}
-				});
-			} else {
-				alert("Error! " + response.message);
-			}
-		})
-	}
+  $scope.add = function() {
+    $scope.loading = true;
+    $http.post(API_URL + "/student/new", {
+        id: $scope.id,
+        nickname: $scope.nickname,
+        name: $scope.name,
+        gender: $scope.gender,
+        phone: $scope.phone,
+        phone2: $scope.phone2,
+        address: $scope.address,
+        facebook: $scope.facebook,
+      })
+      .success(function(response) {
+        $scope.loading = false;
+        if (response.success) {
+          alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
+          $rootScope.preloader = true;
+          $http.get(API_URL + "/student")
+            .success(function(response) {
+              $rootScope.preloader = false;
+              $state.go("studentList");
+              if (response.success) {
+                $rootScope.students = response.data.students;
+              }
+            });
+        } else {
+          alert("Error! " + response.message);
+        }
+      })
+  }
 });
 
 app.controller("studentViewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state, $stateParams) {
-	$scope.id = $stateParams.id;
-	$scope.student = {};
-	$scope.event = {};
-	$scope.myEvents = [];
+  $scope.id = $stateParams.id;
+  $scope.student = {};
 
-	$scope.$watch('$root.students', function() {
-		angular.forEach($rootScope.students, function(student) {
-			if (student.id == $scope.id)
-				$scope.student = student;
-		});
-	});
-
-	$scope.$watch('$root.events', function() {
-		angular.forEach($rootScope.events, function(event) {
-			var myEvent = {
-				id: event.id,
-				name: event.name,
-				description: event.description,
-				created: event.created,
-				status: 0,
-				remark: null,
-				addtime: null,
-			};
-			angular.forEach(event.participants, function(participant) {
-				if (participant.student_id == $scope.id) {
-					myEvent.status = participant.status;
-					myEvent.remark = participant.remark;
-					if (participant.status)
-						myEvent.addtime = participant.addtime;
-				}
-			});
-			$scope.myEvents.push(myEvent);
-		});
-	});
+  $scope.$watch('$root.students', function() {
+    angular.forEach($rootScope.students, function(student) {
+      if (student.id == $scope.id)
+        $scope.student = student;
+    });
+  });
 
 });
 
 app.controller("eventListCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
-	$scope.getParticipantsLength = function(participants) {
-		var i = 0;
-		angular.forEach(participants, function(participant) {
-			if (participant.status)
-				i++;
-		});
-		return i;
-	}
+  $scope.getParticipantsLength = function(participants) {
+    var i = 0;
+    angular.forEach(participants, function(participant) {
+      if (participant.status)
+        i++;
+    });
+    return i;
+  }
 });
 
 app.controller("eventNewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
-	$scope.add = function() {
-		$scope.loading = true;
-		$http.post(API_URL + "/event/new", {
-			name: $scope.name,
-			description: $scope.description,
-			password: $scope.password
-		})
-		.success(function(response) {
-			$scope.loading = false;
-			if (response.success) {
-				alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
-				$rootScope.preloader = true;
-				$http.get(API_URL + "/event")
-				.success(function(response) {
-					$rootScope.preloader = false;
-					$state.go("eventList")
-					if (response.success) {
-						$rootScope.events = response.data.events;
-					}
-				});
-			} else {
-				alert("Error! " + response.message);
-			}
-		})
-	}
+  $scope.add = function() {
+    $scope.loading = true;
+    $http.post(API_URL + "/event/new", {
+        name: $scope.name,
+        description: $scope.description,
+        password: $scope.password
+      })
+      .success(function(response) {
+        $scope.loading = false;
+        if (response.success) {
+          alert("เพิ่มข้อมูลเรียบร้อยแล้ว!");
+          $rootScope.preloader = true;
+          $http.get(API_URL + "/event")
+            .success(function(response) {
+              $rootScope.preloader = false;
+              $state.go("eventList")
+              if (response.success) {
+                $rootScope.events = response.data.events;
+              }
+            });
+        } else {
+          alert("Error! " + response.message);
+        }
+      })
+  }
 });
 
 app.controller("eventViewCtrl", function($rootScope, $scope, $http, API_URL, $window, $state, $stateParams) {
-	$scope.id = $stateParams.id;
-	$scope.event = {};
+  $scope.id = $stateParams.id;
+  $scope.event = {};
 
-	$scope.getRemark = function(id) {
-		var remark = null;
-		angular.forEach($scope.event.participants, function(participant) {
-			if (participant.student_id == id)
-				remark = participant.remark;
-		});
-		return remark;
-	}
-	$scope.getStatus = function(id) {
-		var status = 0;
-		angular.forEach($scope.event.participants, function(participant) {
-			if (participant.student_id == id)
-				status = participant.status;
-		});
-		return status;
-	}
-	$scope.getAddTime = function(id) {
-		var addtime = null;
-		angular.forEach($scope.event.participants, function(participant) {
-			if (participant.student_id == id && participant.status)
-				addtime = participant.addtime;
-		});
-		return addtime;
-	}
-	$scope.check = function(id, status) {
-		var password = null;
-		if ($scope.event.password) {
-			password = prompt("รหัสผ่าน", "");
-			if (!password)
-				return;
-		}
-		$http.post(API_URL + "/event/" + $scope.event.id + "/check", {
-			student_id: id,
-			status: status,
-			password: password,
-		})
-		.success(function(response) {
-			$scope.loading = false;
-			if (response.success) {
-				// $rootScope.preloader = true; reload in background
-				$http.get(API_URL + "/event")
-				.success(function(response) {
-					// $rootScope.preloader = false;
-					if (response.success) {
-						$rootScope.events = response.data.events;
-					}
-				});
-			} else {
-				alert("Error! " + response.message);
-				$state.reload(); // reload checkbox
-			}
-		})
-	}
-	$scope.remark = function(id) {
-		var password = null,
-			remark = null;
-		if ($scope.event.password) {
-			password = prompt("รหัสผ่าน", "");
-			if (!password)
-				return;
-		}
-		if (!(remark = prompt("หมายเหตุ", "")))
-			remark = null;
-
-		$http.post(API_URL + "/event/" + $scope.event.id + "/remark", {
-			student_id: id,
-			remark: remark,
-			password: password,
-		})
-		.success(function(response) {
-			$scope.loading = false;
-			if (response.success) {
-				// $rootScope.preloader = true; reload in background
-				$http.get(API_URL + "/event")
-				.success(function(response) {
-					// $rootScope.preloader = false;
-					if (response.success) {
-						$rootScope.events = response.data.events;
-					}
-				});
-			} else {
-				alert("Error! " + response.message);
-				$state.reload(); // reload checkbox
-			}
-		})
-	}
-
-	$scope.$watch('$root.events', function() {
-		angular.forEach($rootScope.events, function(event) {
-			if (event.id == $scope.id)
-				$scope.event = event;
-		});
-	});
+  $scope.$watch('$root.events', function() {
+    angular.forEach($rootScope.events, function(event) {
+      if (event.id == $scope.id)
+        $scope.event = event;
+    });
+  });
 });
 
 
 app.controller("summaryCtrl", function($rootScope, $scope, $http, API_URL, $window, $state) {
 
-	$scope.joinedThisEvent = function(student_id, event_Id) {
-		var status = 0;
-		angular.forEach($rootScope.events, function(event) {
-			if (event.id == event_Id) {
-				angular.forEach(event.participants, function(participant) {
-					if (participant.student_id == student_id) {
-						status = participant.status;
-					}
-				});
-			}
-		});
-		return status;
-	};
 
 });
--- server.js
+++ server.js
@@ -23,15 +23,17 @@ var conn = mysql.createPool({
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 app.use(function(req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
-	res.setHeader('Access-Control-Allow-Credentials', true);
-	req.ipv4 = req.headers['x-real-ip'] || req.headers['x-client-ip'] || req.headers['x-forwarded-for'] || '127.0.0.1';
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  req.ipv4 = req.headers['x-real-ip'] || req.headers['x-client-ip'] || req.headers['x-forwarded-for'] || '127.0.0.1';
+  next();
 });
 
 app.get('/student', (req, res) => {
@@ -61,7 +63,7 @@ app.get('/student', (req, res) => {
     res.json({
       success: true,
       data: {
-          students: students
+        students: students
       }
     })
   })
@@ -87,12 +89,11 @@ app.post('/student/new', (req, res) => {
       })
     },
     (callback) => {
-      connection.query('insert into student (id, nickname, name, gender, phone, phone2, address, facebook, registered) values (?, ?, ?, ?, ?, ?, ?, ?, now())',
-      [ id, nickname, name, gender, phone, phone2, address, facebook ],
-      (err, response) => {
-        if (err) return callback(err);
-        callback();
-      })
+      connection.query('insert into student (id, nickname, name, gender, phone, phone2, address, facebook, registered) values (?, ?, ?, ?, ?, ?, ?, ?, now())', [id, nickname, name, gender, phone, phone2, address, facebook],
+        (err, response) => {
+          if (err) return callback(err);
+          callback();
+        })
     }
   ], (err) => {
     if (connection) connection.release();
@@ -147,19 +148,19 @@ app.get('/event', (req, res) => {
       event.password = (event.password != null) // Use boolean instead of showing password
       event.participants = [];
       participants.forEach((participant) => {
-        if (participant.event_id == event.id)
+        if (participant.eventId == event.id)
           event.participants.push({
-            student_id: participant.student_id,
+            studentId: participant.studentId,
             status: participant.status,
             remark: participant.remark,
             addtime: participant.addtime
-          }); // Unnescessary to push event_id
+          }); // Unnescessary to push eventId
       })
     })
     res.json({
       success: true,
       data: {
-          events: events
+        events: events
       }
     })
   })
@@ -180,12 +181,11 @@ app.post('/event/new', (req, res) => {
       })
     },
     (callback) => {
-      connection.query('insert into event (name, description, password, created) values (?, ?, ?, now())',
-        [ name, description, password ],
-      (err, response) => {
-        if (err) return callback(err);
-        callback();
-      })
+      connection.query('insert into event (name, description, password, created) values (?, ?, ?, now())', [name, description, password],
+        (err, response) => {
+          if (err) return callback(err);
+          callback();
+        })
     }
   ], (err) => {
     if (connection) connection.release();
@@ -199,11 +199,10 @@ app.post('/event/new', (req, res) => {
   })
 })
 
-app.post('/event/:id/:type(check|remark)', (req, res) => {
+app.post('/event/:id/update', (req, res) => {
   var connection,
     id = req.params.id,
-    type = req.params.type,
-    student_id = req.body.student_id,
+    studentId = req.body.studentId,
     status = req.body.status,
     remark = req.body.remark,
     password = req.body.password;
@@ -216,25 +215,16 @@ app.post('/event/:id/:type(check|remark)', (req, res) => {
       })
     },
     (callback) => {
-      connection.query('select * from event where id = ?',
-        [ id ],
-      (err, response) => {
-        if (err) return callback(err);
-        if (!response.length) return callback('ไม่พบ Event ดังกล่าว');
-        if (response[0].password && response[0].password != password) return callback('Password ไม่ถูกต้อง');
-        callback();
-      })
+      connection.query('select * from event where id = ?', [id],
+        (err, response) => {
+          if (err) return callback(err);
+          if (!response.length) return callback('ไม่พบ Event ดังกล่าว');
+          if (response[0].password && response[0].password != password) return callback('Password ไม่ถูกต้อง');
+          callback();
+        })
     },
     (callback) => {
-      if (type == 'check')
-        connection.query('insert into participant (event_id, student_id, status, addtime) values (?, ?, ?, now()) on duplicate key update status = ?',
-          [ id, student_id, status, status ],
-        (err, response) => {
-          callback(err || null);
-        });
-      else if (type == 'remark')
-        connection.query('insert into participant (event_id, student_id, remark, addtime) values (?, ?, ?, now()) on duplicate key update remark = ?',
-          [ id, student_id, remark, remark ],
+      connection.query('insert into participant (eventId, studentId, status, remark, addtime) values (?, ?, ?, ?, now()) on duplicate key update status = ?, remark = ?', [id, studentId, status, remark, status, remark],
         (err, response) => {
           callback(err || null);
         });
@@ -245,7 +235,7 @@ app.post('/event/:id/:type(check|remark)', (req, res) => {
       success: false,
       message: err
     })
-    console.log(student_id + ' update event#' + id + ' (status = ' + status + ', remark = ' + remark + ')')
+    console.log(studentId + ' update event#' + id + ' (status = ' + status + ', remark = ' + remark + ')')
     res.json({
       success: true
     })
